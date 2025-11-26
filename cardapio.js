75811e2abd932ebf3d071c8c8a6abdc008c911f5
Roger Guilherme Da Silva <rogerguilhermedasilva31@gmail.com>
2025-11-26 20:18:39 -0300

    criando items com js

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,29 @@
-alert('oi bb')
+
+function criarItemCardapio(titulo, descricao, foto) {
+    const divItemCardapio = document.createElement('div')
+    divItemCardapio.className = 'item-cardapio'
+
+    const h3Titulo = document.createElement('h3')
+    h3Titulo.textContent = titulo
+
+    const pDescricao = document.createElement('p')
+    pDescricao.textContent = descricao
+    pDescricao.className = 'descricao'
+
+    const img = document.createElement('img')
+    img.src = foto
+    img.className = 'img-item'
+
+    // Adicionando os elementos na div Mãe
+    const divC = document.getElementById('cardapio')
+
+    divItemCardapio.appendChild(h3Titulo)
+    divItemCardapio.appendChild(pDescricao)
+    divItemCardapio.appendChild(img)
+
+    divC.appendChild(divItemCardapio)
+}
+criarItemCardapio(
+    'bolo de chocolate', 'Um Classico irresistivel com camadas de chocolate', 'https://comidinhasdochef.com/wp-content/uploads/2019/08/Bolo-de-Chocolate-sem-Batedeira.jpg'
+)
+
