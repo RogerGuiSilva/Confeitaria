e2a2fd273a094956b771f7ca7baef3f68054cfa3
Roger Guilherme Da Silva <rogerguilhermedasilva31@gmail.com>
2025-12-03 23:24:31 -0300

    implementação da api

--- cardapio.js
+++ cardapio.js
@@ -1,39 +1,20 @@
+async function carregarCardapio() {
+  try {
+    const resposta = await fetch("https://confeitariaapi.onrender.com");
 
-function criarItemCardapio(titulo, descricao, foto) {
-    const divItemCardapio = document.createElement('div')
-    divItemCardapio.className = 'item-cardapio'
+    if (!resposta.ok) {
+      throw new Error("Erro ao buscar bolos: " + resposta.status);
+    }
 
-    const h3Titulo = document.createElement('h3')
-    h3Titulo.textContent = titulo
+    const lista = await resposta.json();
+    console.log("Dados recebidos:", lista);
 
-    const pDescricao = document.createElement('p')
-    pDescricao.textContent = descricao
-    pDescricao.className = 'descricao'
+    lista.forEach(item => {
+  criarItemCardapio(item.nome, item.descricao, item.foto);
+});
 
-    const img = document.createElement('img')
-    img.src = foto
-    img.className = 'img-item'
 
-    
-    const divC = document.getElementById('cardapio')
-
-    divItemCardapio.appendChild(h3Titulo)
-    divItemCardapio.appendChild(pDescricao)
-    divItemCardapio.appendChild(img)
-
-    divC.appendChild(divItemCardapio)
+  } catch (erro) {
+    console.error("Falha ao carregar cardápio:", erro);
+  }
 }
-criarItemCardapio(
-    'bolo de chocolate', 'Um Classico irresistivel com camadas de chocolate', 'https://comidinhasdochef.com/wp-content/uploads/2019/08/Bolo-de-Chocolate-sem-Batedeira.jpg'
-)
-
-criarItemCardapio(
-    '>Bolo de Morango', 'Fresco e Frutado estilo Pica Pau', 'https://i.pinimg.com/564x/c1/1c/a7/c11ca74d5d3c447e9c43d3283fca3ee1.jpg'
-
-)
-
-criarItemCardapio(
-    '>Bolo de Cenoura', 'bolo que lembra um café da manhã com a vovó', 'https://bolosparavender.com/wp-content/uploads/2025/04/cobertura-para-bolo-de-cenoura-simples.jpg'
-)
-
-
